7042c1c0f288860c3731ab1de993d359ac3e9012
Benjaminxy <benyaamin2@gmail.com>
2026-08-26 22:58:26 +0200

    add Size

--- src/main/java/Repository/LoanRepository.java
+++ src/main/java/Repository/LoanRepository.java
@@ -3,11 +3,13 @@ package Repository;
 import model.Loan;
 
 public class LoanRepository {
-    Loan [] loans = new Loan[100];
+   private static final Loan [] loans = new Loan[100];
+   private static int size;
 
 public void save(Loan loan) {
     for (int i = 0; i < loans.length; i++) {
         if (loans[i] == null) {
+            size++;
             loans[i] = loan;
             break;
         }
